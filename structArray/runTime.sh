04a9a89f27edcbdb1cfb001dda86db20f1df67dc
Brent Hall <hallba@umail.iu.edu>
2016-12-08 15:12:44 -0500

    fixed array.c

---
+++ structArray/runTime.sh
@@ -0,0 +1,14 @@
+res1=$(date +%s.%N)
+
+python test.py
+
+res2=$(date +%s.%N)
+dt=$(echo "$res2 - $res1" | bc)
+dd=$(echo "$dt/86400" | bc)
+dt2=$(echo "$dt-86400*$dd" | bc)
+dh=$(echo "$dt2/3600" | bc)
+dt3=$(echo "$dt2-3600*$dh" | bc)
+dm=$(echo "$dt3/60" | bc)
+ds=$(echo "$dt3-60*$dm" | bc)
+
+printf "Total runtime: %02d:%02.4f\n" $dm $ds
